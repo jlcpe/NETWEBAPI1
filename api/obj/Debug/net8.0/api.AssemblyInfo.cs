b331b60fc174da5f875a845133dfa069c999e428
JLJL <jaylordlubin@gmail.com>
2024-07-04 19:28:28 +0800

    added model

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f619759a77014159f8ed9405e09a0b972151d144")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d33c0542c94bfa7d21875f171e3846b630e7960")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
